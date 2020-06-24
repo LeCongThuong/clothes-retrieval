e3e15b220921d166980fca018aeb0ee89a9c2790
LeCongThuong <thuongle276@gmail.com>
2020-06-24 16:58:07 +0700

    change densenet layers

--- backbones/densenet_models.py
+++ backbones/densenet_models.py
@@ -4,7 +4,7 @@ from torchvision.models import densenet121, densenet161
 
 
 class DenseNetModels(nn.Module):
-    def __init__(self, embedding_dim, num_classes, image_size, archi="densenet121", pretrained=True, dropout=0):
+    def __init__(self, embedding_dim, num_classes, image_size, archi="densenet121", pretrained=True, dropout=0.4, alpha=10, gap=1):
         super(DenseNetModels, self).__init__()
         if archi == "densenet121":
             self.model = densenet121(pretrained=pretrained)
@@ -16,26 +16,43 @@ class DenseNetModels(nn.Module):
         self.model.classifier = nn.Linear(self.output_conv, self.embedding_dim)
         self.dropout = nn.Dropout(p=dropout)
         self.pooling_layer = nn.AdaptiveAvgPool2d(1)
-        self.bn = nn.BatchNorm1d(self.embedding_dim)
-        self.model.classification_layer = nn.Linear(
-            self.embedding_dim, num_classes)
+        self.alpha = alpha
+        self.gap = gap
+
+    def l2_norm(self, input):
+        input_size = input.size()
+        buffer = torch.pow(input, 2)
+        normp = torch.sum(buffer, 1).add_(1e-10)
+        norm = torch.sqrt(normp)
+        _output = torch.div(input, norm.view(-1, 1).expand_as(input))
+        output = _output.view(input_size)
+        return output
 
     def forward(self, x):
         x = self.model.features(x)
-        x = self.pooling_layer(x)
-        x = x.view(x.size(0), -1)
+        if self.gap == 1:
+            x = self.pooling_layer(x)
+            x = x.view(x.size(0), -1)
+
+        elif self.gap == 0:
+            x = x.view(x.size(0), -1)
+            x = self.dropout(x)
+
         x = self.model.classifier(x)
-        x = self.bn(x)
-        return x
+        self.features = self.l2_norm(x)
+
+        # Multiply by alpha = 10 as suggested in https://arxiv.org/pdf/1703.09507.pdf
+        self.features = self.features * self.alpha
+        return self.features
 
     def _get_output_conv(self, shape):
         x = torch.rand(shape)
         x = self.model.features(x)
-        output_conv_shape = x.size(1)
+        if self.gap == 1:
+            output_conv_shape = x.size(1)
+        elif self.gap == 0:
+            x = x.view(x.size(0), -1)
+            output_conv_shape = x.size(1)
         return output_conv_shape
 
-    def forward_classifier(self, x):
-        features = self.forward(x)
-        features = self.dropout(features)
-        logits = self.model.classification_layer(features)
-        return features, logits
+
